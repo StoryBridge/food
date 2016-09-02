33b22b3d99ed3f95ef8e0c217a45817841b81ae3
Window <Window@DESKTOP-42NRJNS>
2016-09-02 11:50:01 +0900

    z

--- FoodDamas/src/main/java/org/food/persistence/MemberDAOImpl.java
+++ FoodDamas/src/main/java/org/food/persistence/MemberDAOImpl.java
@@ -15,30 +15,28 @@ public class MemberDAOImpl implements MemberDAO {
 	@Inject
 	private SqlSessionTemplate sqlsession;
 	
-	
+	//회원가입
 	@Override
 	public void insert(MemberVO vo) throws Exception {
 		// TODO Auto-generated method stub
 		sqlsession.insert("org.food.persistence.MemberMapper.insert", vo);
-		
 	}
 
-
+	//회원보기
 	@Override
 	public MemberVO readOne(String u_id) throws Exception {
 		// TODO Auto-generated method stub
 		return sqlsession.selectOne("org.food.persistence.MemberMapper.readOne", u_id);
 	}
 
-
+	//회원수정
 	@Override
 	public void update(MemberVO vo) throws Exception {
 		// TODO Auto-generated method stub
 		sqlsession.update("org.food.persistence.MemberMapper.update", vo);
-		
 	}
 
-
+	//회원삭제
 	@Override
 	public void delete(String u_id) throws Exception {
 		// TODO Auto-generated method stub
@@ -46,18 +44,23 @@ public class MemberDAOImpl implements MemberDAO {
 		
 	}
 
-
+	//승인대기자 리스트
 	@Override
-	public List<MemberVO> listAll(String u_id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<MemberVO> listState(Integer register_state) throws Exception {
+		return sqlsession.selectList("org.food.persistence.MemberMapper.listState", register_state);
 	}
 
-
+	//페이지
 	@Override
 	public Integer pageCount() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return sqlsession.selectOne("org.food.persistence.MemberMapper.pageCount");
+	}
+
+	//승인대기자 승인
+	@Override
+	public void updateState(MemberVO vo) throws Exception {
+		sqlsession.update("org.food.persistence.MemberMapper.updateState", vo);
 	}
 
 }
