420b04051098aeccb0cafea2dbc573368933684e
StoryBridge <zzennam@naver.com>
2016-09-02 15:46:31 +0900

    StatementDAO, Imple, Test 추가

---
+++ FoodDamas/src/main/java/org/food/persistence/StatementDAO.java
@@ -0,0 +1,14 @@
+package org.food.persistence;
+
+import org.food.domain.SalesVO;
+import java.util.List;
+
+public interface SalesDAO {
+	
+	public void insert(SalesVO vo);
+	public List<SalesVO> read(String u_id);
+	public void update(String u_id);
+	
+	public SalesVO readSalesOne(String u_id);
+
+}
---
+++ FoodDamas/src/main/java/org/food/persistence/StatementDAOImpl.java
@@ -0,0 +1,44 @@
+package org.food.persistence;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.food.domain.SalesVO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public class SalesDAOImpl implements SalesDAO {
+
+	@Inject
+	private SqlSessionTemplate sqlsession;
+
+	@Override
+	public void insert(SalesVO vo) {
+		// TODO Auto-generated method stub
+		sqlsession.insert("org.food.persistence.SalesMapper.insert",vo);
+
+	}
+
+	@Override
+	public List<SalesVO> read(String u_id) {
+		// TODO Auto-generated method stub
+		return 		sqlsession.selectList("org.food.persistence.SalesMapper.read",u_id);
+	}
+
+	@Override
+	public void update(String u_id) {
+		// TODO Auto-generated method stub
+		sqlsession.update("org.food.persistence.SalesMapper.update",u_id);
+
+
+	}
+	@Override
+	public SalesVO readSalesOne(String u_id) {
+		// TODO Auto-generated method stub
+		return sqlsession.selectOne("org.food.persistence.SalesMapper.readOne", u_id);
+	}
+
+}
